7ab694c086af54965cc6a817a9995ab90e110876
Matheus Schossler <matheus.schossler@gmail.com>
2022-08-15 21:28:26 -0500

    cleaned main file

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import multiprocessing
-import os
 import time
 
 import pandas as pd
 from numpy import linalg as npla
 
-from config import aux_dir_path, namecsv, itmax, nprocesses, t0, title, bands
+from config import aux_dir_path, namecsv, itmax, nprocesses, t0, bands
 from input.parameters import *
 from model.densities import rho0
 from model.hamiltonians import full_hp, full_hm, full_hpm, full_hmp, h0p2, h0m2, h0p, h0m, mZm, idp, idps, idm, idms
-from utils import eigen, frange, idxcalc, df_round, sort_dict, observable_to_csv, idxcalc_base
+from utils import eigen, frange, df_round, sort_dict, observable_to_csv, idxcalc_base
 
 
 def hp(n, nprime, s1, s2):
@@ -39,18 +38,13 @@ def loopU(u):
     eigenvaluem = [h0m(u)[0][0]] + [h0m(u)[1][1]] + eigenvaluem2.tolist()
 
     h0 = np.diag(eigenvaluep + eigenvaluem + eigenvaluep + eigenvaluem)
-    # print('here')
-    # mZm = k * np.diag([-Zm for i in range(8)] + [Zm for i in range(8)])
-
 
     eigenvectorp = np.array([[1, 0, 0, 0], [0, 1, 0, 0]] + [[0, 0] + x for x in eigenvectorp2.tolist()])
     eigenvectorm = np.array([[1, 0, 0, 0], [0, 1, 0, 0]] + [[0, 0] + x for x in eigenvectorm2.tolist()])
 
-
     it = 1;
     while it < itmax:
         deltatb = sum([rho[idp(n)][idp(n)] + rho[idps(n)][idps(n)] - rho[idm(n)][idm(n)] - rho[idms(n)][idms(n)] for n in setH])
-
         # hphpm = [[hp(n, nprime) for n in setH] + [hpm(n, nprime) for n in setH] for nprime in setH]
         # hmphm = [[hmp(n, nprime) for n in setH] + [hm(n, nprime) for n in setH] for nprime in setH]
         hphpm = [[hp(n, nprime, 1, 1) for nprime in setH] + [hpm(n, nprime, 1, 1) for nprime in setH] for n in setH]
@@ -74,17 +68,13 @@ def loopU(u):
         Hintdownup = np.vstack((hphpmsdu, hmphmsdu))
 
         Hint = k * np.vstack((np.hstack((Hintup, Hintupdown)), np.hstack((Hintdownup, Hintdown))))
-        # Hint = k * np.vstack((np.hstack((Hintup, Hintupdown)), np.hstack((Hintupdown, Hintdown))))
         H = Hint + h0 + mZm  # np.add(Hint, h0)
         eigenvalue_loop, eigenvector_loop = eigen(H);
         rho = sum(np.outer(eigenvector_loop[i, :], eigenvector_loop[i, :]) for i in range(nu));
 
         it += 1
 
-
-
     eigenvalue, eigenvector = npla.eig(H)
-
     idxfunc = np.argsort(eigenvalue)
     #
     eigenvalue = eigenvalue[idxfunc]
@@ -101,41 +91,34 @@ def loopU(u):
     Et = sum([eigenvalue[i] for i in range(nu)]) + ehf
 
     dict_quantities_u = {'u': u * 1e3,
-                  'eigenvalue': 1e3 * eigenvalue,
-                  'eigenvector': eigenvector,
-                  'Et': 1e3 * Et,
-                  'h0': df_round(1e3 * h0),
-                  'rhoU': df_round(rho),
-                  'Eh_deltaU': 1e3 * k * Eh * deltatb,
-                  'Hint': df_round(1e3 * Hint),
-                  }
-
+                         'eigenvalue': 1e3 * eigenvalue,
+                         'eigenvector': eigenvector,
+                         'Et': 1e3 * Et,
+                         'h0': df_round(1e3 * h0),
+                         'rhoU': df_round(rho),
+                         'Eh_deltaU': 1e3 * k * Eh * deltatb,
+                         'Hint': df_round(1e3 * Hint),
+                         }
     return dict_quantities_u
 
-a_pool = multiprocessing.Pool(processes=nprocesses)
 
+a_pool = multiprocessing.Pool(processes=nprocesses)
 quantities = a_pool.map(loopU, frange(U0minD, U0maxD, dU0D))
 
-# print(time.time() - t0)
-
 quantities_dict = {}
-
 for dict in quantities:
     quantities_dict[dict['u']] = dict
 
 quantities_dict = sort_dict(quantities_dict)
 
-energies=[]
-for k,v in quantities_dict.items():
-    u_temp , eigenvalue_temp, eigenvector_temp = v['u'], v['eigenvalue'], v['eigenvector']
+energies = []
+for k, v in quantities_dict.items():
+    u_temp, eigenvalue_temp, eigenvector_temp = v['u'], v['eigenvalue'], v['eigenvector']
     idx = idxcalc_base(eigenvector_temp, u_temp)
     energies.append([u_temp] + eigenvalue_temp[idx].tolist())
 
-energies_df = pd.DataFrame(energies)
-energies_df.columns = ['u'] + bands
+energies_df = pd.DataFrame(energies, columns=['u'] + bands)
 energies_df.to_csv(aux_dir_path + namecsv, index=False)
-
-# print(len(quantities_dict))
 
 observable_to_csv(quantities_dict, 'h0')
 observable_to_csv(quantities_dict, 'rhoU')
@@ -144,7 +127,5 @@ observable_to_csv(quantities_dict, 'Hint')
 observable_to_csv(quantities_dict, 'Et')
 
 print(time.time() - t0)
-
 print('file ' + namecsv + ' saved')
-# print('done')
 print(" \n done in ", time.time() - t0)
