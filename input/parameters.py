4b5ed7bbe8943fd4268aeecc320902c46c6bed5c
Matheus Schossler <matheus.schossler@gmail.com>
2022-09-17 22:44:47 -0500

    congergin CAF phase parameters for both no_LL2_mixing_and_asym and full_range models - run in the server

--- input/parameters.py
+++ input/parameters.py
@@ -33,7 +33,7 @@ if not apha_H_asym_small_u:
     uperp = 0
 
 U0minD = -10e-3
-U0maxD = 30e-3
+U0maxD = 10e-3
 dU0D = 1e-3
 
 u_zero = 1
@@ -45,7 +45,7 @@ save_folder_name = 1  # change this to off when done with tests
 
 itmax_full_range = int(2e2)
 alpha_rand_full_range = 0
-same_rhoRandom = 0
+same_rhoRandom = 1
 alpha_rho = 0  # controls numerical regularization for rho (small memory of rho from previews loop)
 
 alpha_k_nu4 = 0.244
@@ -92,9 +92,9 @@ k = (np.sqrt(np.pi / 2) * el) / (4 * np.pi * ep0 * epr * Lb) * alpha_k
 ######################################################################################################################
 ######################################### hartree_fock_with_asymmetric_interactions.py #############################
 alpha_H_oct_int = 1
-itmax_asymmetric_calcs = int(1e4)
+itmax_asymmetric_calcs = int(1e5)
 alpha_reg_asym_calcs = 1
-alpha_rand_asymmetric_calcs = 0.005  # 0 for Ferro, 0.1 for CAF phase and uperp_meV: -3.2 uz_meV: 14.0
+alpha_rand_asymmetric_calcs = 0.2  # 0 for Ferro, 0.1 for CAF phase and uperp_meV: -3.2 uz_meV: 14.0
 alpha_rho_asymmetric_calcs = 0.0
 ### appoximation mode for LL2 and LLm2 ###
 add_int_to_bands_LLm2_LL2_low_u = 1  # if false this is effectivelly equivalent to fast_none_interact mode for low u regime
