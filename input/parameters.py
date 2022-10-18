21618edc14b9046fc7ace98cb3f92a6f499f103c
Matheus Schossler <matheus.schossler@gmail.com>
2022-10-18 15:35:53 -0500

    updatign parameters.py to study nu=0.

--- input/parameters.py
+++ input/parameters.py
@@ -32,11 +32,11 @@ if (not apha_H_asym) or (abs(nu) > 3):
     uz = 0
     uperp = 0
 
-U0minD = -0.5e-3
-U0maxD = 1.5e-3
-dU0D = 0.5e-3
+U0minD = -10e-3
+U0maxD = 50e-3
+dU0D = 1e-3
 
-add_legend_curve = 0
+add_legend_curve = 1
 
 u_zero = 0.5
 u_zero = round(u_zero, 4)
@@ -47,7 +47,7 @@ save_folder_name = 1  # change this to off when done with tests
 
 itmax_full_range = int(1e1)
 alpha_rand_full_range_small_u = 0
-alpha_rand_full_range_high_u = 0.1
+alpha_rand_full_range_high_u = 0.01
 same_rhoRandom = 1
 alpha_rho = 0.05 * 0  # controls numerical regularization for rho (small memory of rho from previews loop)
 
@@ -107,7 +107,7 @@ k = (np.sqrt(np.pi / 2) * el) / (4 * np.pi * ep0 * epr * Lb) * alpha_k
 model_regime = 'full_range'
 ######################################################################################################################
 ######################################### hartree_fock_with_asymmetric_interactions.py #############################
-model_regime = 'no_LL2_mixing_and_asym'
+# model_regime = 'no_LL2_mixing_and_asym'
 alpha_H_oct_int = 1
 itmax_asymmetric_calcs = int(1e3)
 alpha_reg_asym_calcs = alpha_H_oct_int
