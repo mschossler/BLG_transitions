cae036ad032b4b3012f1ee96f21c8e789656253a
Matheus Schossler <matheus.schossler@gmail.com>
2023-11-16 11:04:04 -0600

    adding parameters and densities for each simulation regime

---
+++ paramenters_and_densities/hf-assym-PLP-nu0/parameters and densities/densities.py
@@ -0,0 +1,210 @@
+import numpy as np
+import pandas as pd
+
+# print('here_density')
+# import time
+# time.sleep(.1)
+
+input_dir = 'input/'
+
+if __name__ == "__main__":
+    # setting path
+    import sys
+
+    sys.path.append('../')
+    input_dir = '../input/'
+
+from input.parameters import nu, alpha_rand_full_range_small_u, alpha_rand_full_range_high_u, alpha_rand_asymmetric_calcs, file_seed  # , seed_large_u  # , model_regime
+from config import bands, tol
+from utils import eigen, remove_small_imag
+
+
+class Density_Seed:
+    def __init__(self, model_regime, nu):
+        self.rho0constUp = None
+        self.rho0constUm = None
+        self.model_regime = model_regime
+        self.nu = nu
+        self.number_occupied_bands = nu + 8
+
+    filling_order_Upositive = ['LLm2_Km_Sdown',  # -7
+                               'LLm2_Km_Sup',  # -6
+                               'LLm2_Kp_Sdown',  # -5
+                               'LLm2_Kp_Sup',  # -4
+                               'LL0_Km_Sdown',  # -3
+                               'LL0_Km_Sup',  # -2
+                               'LL1_Km_Sdown',  # -1
+                               'LL1_Km_Sup',  # 0
+                               'LL0_Kp_Sdown',  # 1
+                               'LL0_Kp_Sup',  # 2
+                               'LL1_Kp_Sdown',  # 3
+                               'LL1_Kp_Sup',  # 4
+                               'LL2_Km_Sdown',  # 5
+                               'LL2_Km_Sup']  # 6
+
+    filling_order_Unegative = ['LLm2_Kp_Sdown',  # -7
+                               'LLm2_Kp_Sup',  # -6
+                               'LLm2_Km_Sdown',  # -5
+                               'LLm2_Km_Sup',  # -4
+                               'LL0_Kp_Sdown',  # -3
+                               'LL0_Kp_Sup',  # -2
+                               'LL1_Kp_Sdown',  # -1
+                               'LL1_Kp_Sup',  # 0
+                               'LL0_Km_Sdown',  # 1
+                               'LL0_Km_Sup',  # 2
+                               'LL1_Km_Sdown',  # 3
+                               'LL1_Km_Sup',  # 4
+                               'LL2_Kp_Sdown',  # 5
+                               'LL2_Kp_Sup']  # 6
+
+    filling_order_small_abs_u = ['LLm2_Kp_Sdown',  # -7
+                                 'LLm2_Km_Sdown',  # -6
+                                 'LLm2_Kp_Sup',  # -5
+                                 'LLm2_Km_Sup',  # -4
+                                 'LL0_Km_Sdown',  # -3
+                                 'LL0_Kp_Sdown',  # -2
+                                 'LL1_Km_Sdown',  # -1
+                                 'LL1_Kp_Sdown',  # 0
+                                 'LL0_Km_Sup',  # 1
+                                 'LL0_Kp_Sup',  # 2
+                                 'LL1_Km_Sup',  # 3
+                                 'LL1_Kp_Sup',  # 4
+                                 'LL2_Km_Sdown',  # 5
+                                 'LL2_Kp_Sdown']  # 6
+
+    # density for small u for best fit on transitions vs nu data
+    # filling_order_small_abs_u = ['LLm2_Kp_Sdown',  # -7
+    #                              'LLm2_Km_Sdown',  # -6
+    #                              'LLm2_Kp_Sup',  # -5
+    #                              'LLm2_Km_Sup',  # -4
+    #                              'LL0_Kp_Sdown',  # -2
+    #                              'LL0_Km_Sdown',  # -3
+    #                              'LL1_Kp_Sdown',  # 0
+    #                              'LL1_Km_Sdown',  # -1
+    #                              'LL1_Km_Sup',  # 3
+    #                              'LL1_Kp_Sup',  # 4
+    #                              'LL0_Km_Sup',  # 1
+    #                              'LL0_Kp_Sup',  # 2
+    #                              'LL2_Km_Sdown',  # 5
+    #                              'LL2_Kp_Sdown']  # 6
+
+    # filling_order_small_abs_u = ['LLm2_Kp_Sdown',  # -7
+    #                              'LLm2_Km_Sdown',  # -6
+    #                              'LLm2_Kp_Sup',  # -5
+    #                              'LLm2_Km_Sup',  # -4
+    #                              'LL1_Km_Sdown',  # -1
+    #                              'LL1_Kp_Sdown',  # 0
+    #                              'LL0_Km_Sdown',  # -3
+    #                              'LL0_Kp_Sdown',  # -2
+    #                              'LL1_Kp_Sup',  # 4
+    #                              'LL0_Kp_Sup',  # 2
+    #                              'LL1_Km_Sup',  # 3
+    #                              'LL0_Km_Sup',  # 1
+    #                              'LL2_Km_Sdown',  # 5
+    #                              'LL2_Kp_Sdown']  # 6
+    ### Orbital Polarized seed
+    filling_order_small_abs_u = ['LLm2_Kp_Sdown',  # -7
+                                 'LLm2_Km_Sdown',  # -6
+                                 'LLm2_Kp_Sup',  # -5
+                                 'LLm2_Km_Sup',  # -4
+                                 'LL0_Km_Sdown',  # -3
+                                 'LL0_Kp_Sdown',  # -2
+                                 'LL0_Km_Sup',  # -1
+                                 'LL0_Kp_Sup',  # 0
+                                 'LL1_Km_Sdown',  # 1
+                                 'LL1_Kp_Sdown',  # 2
+                                 'LL1_Km_Sup',  # 3
+                                 'LL1_Kp_Sup',  # 4
+                                 'LL2_Km_Sdown',  # 5
+                                 'LL2_Kp_Sdown']  # 6
+
+    # base_octet = ['LL0_Km_Sdown', 'LL0_Kp_Sdown', 'LL1_Km_Sdown', 'LL1_Kp_Sdown', 'LL0_Km_Sup', 'LL0_Kp_Sup', 'LL1_Km_Sup', 'LL1_Kp_Sup']
+    # seed_oct_dict = {-4: (0, 0, 0, 0, 0, 0, 0, 0),
+    #                  -3: (1, 0, 0, 0, 0, 0, 0, 0),
+    #                  -2: (1, 1, 0, 0, 0, 0, 0, 0),
+    #                  -1: (1, 1, 1, 0, 0, 0, 0, 0),
+    #                  0: (1, 1, 1, 1, 0, 0, 0, 0),
+    #                  1: (1, 1, 1, 1, 1, 0, 0, 0),
+    #                  2: (1, 1, 1, 1, 1, 1, 0, 0),
+    #                  3: (1, 1, 1, 1, 1, 1, 1, 0),
+    #                  4: (1, 1, 1, 1, 1, 1, 1, 1)
+    #                  }
+
+    def ramdom_16x16_density(self):
+        if (self.model_regime == 'full_range') and (self.nu == 0) and file_seed:
+            rhorand8 = pd.read_csv(input_dir + 'rho0phbroken.csv', header=None).values.tolist()
+
+            rho0file8_zero = np.pad(rhorand8, ((0, 8), (0, 8)), mode='constant')
+            zero_rho0file8 = np.pad(rhorand8, ((8, 0), (8, 0)), mode='constant')
+
+            rhorand16 = rho0file8_zero + zero_rho0file8
+        else:
+            rhorand8 = np.random.rand(8, 8)
+            eigenvalue, eigenvector = eigen(rhorand8)
+            rhorand8 = np.real(sum(np.outer(eigenvector[i, :], eigenvector[i, :]) for i in range(self.number_occupied_bands // 2)))
+
+            rhorand8_zero = np.pad(rhorand8, ((0, 8), (0, 8)), mode='constant')
+            zero_rhorand8 = np.pad(rhorand8, ((8, 0), (8, 0)), mode='constant')
+
+            rhorand16 = rhorand8_zero + zero_rhorand8
+
+        return rhorand16
+
+    def diag_full_regime_high_u(self, u_signal):
+
+        if u_signal >= 0:
+            filling_order = self.filling_order_Upositive
+        if u_signal < 0:
+            filling_order = self.filling_order_Unegative
+        diag = [(1 if band in filling_order[0:self.number_occupied_bands] else 0) for band in bands]
+        if abs(sum(diag) - self.number_occupied_bands) > tol:
+            print('Wrong filling factor for nu %i' % self.nu)
+            exit()
+
+        rhorand16 = self.ramdom_16x16_density()
+        return (1 - alpha_rand_full_range_high_u) * np.diag(diag) + alpha_rand_full_range_high_u * rhorand16
+
+    def diag_full_regime_small_u(self):
+
+        diag = [(1 if band in self.filling_order_small_abs_u[0:self.number_occupied_bands] else 0) for band in bands]
+        if abs(sum(diag) - self.number_occupied_bands) > tol:
+            print('Wrong filling factor for nu %i' % self.nu)
+            exit()
+
+        rhorand16 = self.ramdom_16x16_density()
+        if self.model_regime == 'full_range':
+            alpha_rand = alpha_rand_full_range_small_u
+        elif self.model_regime == 'no_LL2_mixing_and_asym':
+            alpha_rand = alpha_rand_asymmetric_calcs
+        return (1 - alpha_rand) * np.diag(diag) + alpha_rand * rhorand16
+
+    # def seed_asymmetric_calcs(self):
+    #
+    #     diag = [(1 if band in self.filling_order_small_abs_u[0:self.number_occupied_bands] else 0) for band in bands]
+    #     if abs(sum(diag) - number_occupied_bands) > tol:
+    #         print('Wrong filling factor for nu %i' % self.nu)
+    #         exit()
+    #     rhorand16 = self.ramdom_16x16_density()
+    #
+    #     rho0const = (1 - alpha_rand_asymmetric_calcs) * np.diag(diag) + alpha_rand_asymmetric_calcs * rhorand16
+    #
+    #     return rho0const
+
+    def assign_densities(self):
+        self.rho0const_small_u = remove_small_imag(self.diag_full_regime_small_u())
+        self.rhoRandom = self.ramdom_16x16_density()
+        if self.model_regime == 'full_range':
+            self.rho0constUp = remove_small_imag(self.diag_full_regime_high_u(+1))
+            self.rho0constUm = remove_small_imag(self.diag_full_regime_high_u(
+                -1))  # elif self.model_regime == 'no_LL2_mixing_and_asym':  #     self.rho0constUp = remove_small_imag(self.seed_asymmetric_calcs())  #     self.rho0constUm = remove_small_imag(self.seed_asymmetric_calcs())
+
+
+def density_by_model_regime(model_regime):
+    densities = Density_Seed(model_regime, nu)
+    densities.assign_densities()
+    rho0constUp = densities.rho0constUp
+    rho0constUm = densities.rho0constUm
+    rhoRandom = densities.rhoRandom
+    rho0const_small_u = densities.rho0const_small_u
+
+    return {'rho0constUp': rho0constUp, 'rho0constUm': rho0constUm, 'rho0const_small_u': rho0const_small_u, 'rhoRandom': rhoRandom}
---
+++ paramenters_and_densities/none-int/paremeters/nu0/parameters.py
@@ -0,0 +1,178 @@
+import sys
+
+import numpy as np
+
+nu_default = 0
+
+if len(sys.argv) == 2:
+    nu = int(sys.argv[1])
+else:
+    nu = nu_default
+
+if abs(nu) > 6:
+    print('Error: nu (filling factor) out of range')
+    exit()
+else:
+    print('executing script with nu = %i' % nu)
+number_occupied_bands = nu + 8  # number_occupied_bands = 8 is the charge neutrality point
+
+u_critical = 9e-3  # value of u in eV for phase transition based on experiment data with screening factor of 0.26 for nu=0
+asym = 1
+alpha_Zm = 1  # 0.04227165829987071 # k / alpha_k
+alpha_int_H = 0 # 0 for none int calculations on the full_range model
+apha_H_asym = 0
+potential_asym_layers = 0
+
+alpha_reg = 0
+alpha_x = 1
+uz = 7e-3
+uperp = -2e-3
+Orbital_Polarized_seed = False
+if Orbital_Polarized_seed:
+    uz = 6e-3
+    uperp = -3e-3
+
+if (not apha_H_asym) or (abs(nu) > 1):
+    uz = 0
+    uperp = 0
+
+U0minD = -8.01e-3
+U0maxD = 50.01e-3
+dU0D = 0.5e-3
+
+fraction_part = round(U0minD * 1e3 % 1, 2)
+
+add_legend_curve = 0  # if activated k values must be changed accordingly to possible values of u inside first for loop of plot.plot_energies
+
+u_zero = 0.5
+u_zero = round(u_zero, 4)
+
+tests_mode = 'on'
+save_folder_name = 1  # change this to off when done with tests
+# tests_mode = 'off'
+
+itmax_full_range = int(1e2)
+alpha_rand_full_range_small_u = 0
+alpha_rand_full_range_high_u = 0
+same_rhoRandom = 1
+alpha_rho = 0.05 * 0  # controls numerical regularization for rho (small memory of rho from previews loop)
+
+alpha_k_nu4 = 0.244
+alpha_k_nu0 = 0.26
+
+
+# alpha_k_dic = {i: alpha_k_nu4 for i in range(-6, 7)}
+# alpha_k_dic[0] = alpha_k_nu0
+# alpha_k = alpha_k_dic[nu]
+# def alpha_k_linear(nu):
+#    y = (alpha_k_nu4 - alpha_k_nu0) * nu / 4 + alpha_k_nu0
+#    return round(y, 4)
+def alpha_k_linear(nu):
+    if nu == 0:
+        return alpha_k_nu0
+    elif nu < 0:
+        return 0.285
+    elif nu > 0:
+        return alpha_k_nu4
+
+
+alpha_k = alpha_k_linear(nu)
+
+file_seed = 0
+if file_seed:
+    alpha_rand_full_range_small_u = 0.6
+    # alpha_rand_full_range_small_u = 0.83
+
+print('uz=%(uz).1fmeV, uperp=%(uperp).1fmeV' % {'uz': uz * 1e3, 'uperp': uperp * 1e3})
+
+B = 13
+omega = 35 * np.sqrt(B) / 1e3
+gamma0 = 3
+gamma1 = 0.41
+gamma4 = 0.15 * asym
+eta4 = gamma4 / gamma0
+gamma3 = -0.3 * asym  # gamma3 = 0.3;
+eta3 = gamma3 / gamma0
+Delta = 0.018 * asym
+Delta_ab = 0.001 * 0
+Delta_td = (2 * gamma1 * gamma4 / gamma0 + Delta)
+beta = (omega / gamma1) ** 2
+Zm = 57.9e-6 * B * alpha_Zm  # in eV,  57.9e-6 is from Zhang2011PRB below equation 17 #temperature: 4 K=4 * 0.0862=0.3448meV # k * 0.0178 #
+clight = 299792458
+hbar = 6.62607e-34 / (2 * np.pi)
+el = 1.602176634e-19
+dlayer = 3.35e-10
+ep0 = 8.8541878128e-12
+alpha_tilda = alpha_k * (dlayer * 1e9)
+Lb = np.sqrt(hbar / (el * B))
+x = dlayer / Lb * alpha_x
+epr = 6
+Eh = x / np.sqrt(2 * np.pi)
+k = (np.sqrt(np.pi / 2) * el) / (4 * np.pi * ep0 * epr * Lb) * alpha_k
+couplings_dict = {'gamma0': gamma0, 'gamma1': gamma1, 'gamma4': gamma4, 'gamma3': gamma3, 'Delta': Delta, 'Delta_td': Delta_td, 'k': k}
+model_regime = 'full_range'
+effective_H0 = False
+projected_four_band_H0 = not effective_H0
+######################################################################################################################
+######################################### hartree_fock_with_asymmetric_interactions.py #############################
+# model_regime = 'no_LL2_mixing_and_asym'
+alpha_H_oct_int = 1
+itmax_asymmetric_calcs = int(1e4)
+alpha_reg_asym_calcs = 1
+alpha_rand_asymmetric_calcs = 0  # 0 for Ferro, 0.1 for CAF phase and uperp_meV: -3.2 uz_meV: 14.0
+alpha_rho_asymmetric_calcs = 0.0001
+replace_LLm2_LL2_low_u = 0  # if false this is effectivelly equivalent to fast_none_interact mode for low u regime
+######################################################################################################################
+######################################################################################################################
+parameters_to_save = {'nu': nu,
+                      'number_occupied_bands': number_occupied_bands,
+                      'u_critical_meV': u_critical * 1e3,
+                      'asym': asym,
+                      'alpha_H_oct_int': alpha_H_oct_int,
+                      'alpha_int_H': alpha_int_H,
+                      'apha_H_asym': apha_H_asym,
+                      'alpha_reg_asym_calcs': alpha_reg_asym_calcs,
+                      'file_seed': file_seed,
+                      'screening': alpha_k,
+                      # 'seed_large_u': seed_large_u,
+                      'Zm_meV': Zm * 1e3,
+                      'x': x,
+                      'itmax_full_range': itmax_full_range,
+                      'itmax_asymmetric_calcs': itmax_asymmetric_calcs,
+                      'alpha_rand_full_range_small_u': alpha_rand_full_range_small_u,
+                      'alpha_rand_full_range_high_u': alpha_rand_full_range_high_u,
+                      'alpha_rand_asymmetric_calcs': alpha_rand_asymmetric_calcs,
+                      'alpha_rho': alpha_rho,
+                      'alpha_rho_asymmetric_calcs': alpha_rho_asymmetric_calcs,
+                      'model_regime': model_regime,
+                      'alpha_reg': alpha_reg,
+                      'replace_LLm2_LL2_low_u': replace_LLm2_LL2_low_u,
+                      'alpha_int_H': alpha_int_H,
+                      'uz_meV': uz * 1e3,
+                      'uperp_meV': uperp * 1e3,
+                      'u_zero_meV': u_zero,
+                      'range_meV': (U0minD * 1e3, U0maxD * 1e3, dU0D * 1e3),
+                      'tests_mode': tests_mode,
+                      'save_folder_name': save_folder_name,
+                      'potential_asym_layers': potential_asym_layers,
+                      'couplings_dict': couplings_dict,
+                      'effective_H0': effective_H0,
+                      'Orbital_Polarized_seed': Orbital_Polarized_seed
+                      # 'add_int_to_bands_LLm2_LL2_low_u':add_int_to_bands_LLm2_LL2_low_u
+                      }
+# Zm = k * 0.0178 #use def above, this is slightly off due to alpha_k
+if model_regime == 'full_range':
+    parameters_in_plot_list = ['nu', 'asym', 'screening', 'alpha_int_H', 'apha_H_asym',
+                               'itmax_full_range', 'alpha_rand_full_range_small_u', 'alpha_rand_full_range_high_u', 'alpha_rho', 'alpha_reg', 'uz_meV', 'uperp_meV', 'effective_H0']
+elif model_regime == 'no_LL2_mixing_and_asym':
+    parameters_in_plot_list = ['nu', 'asym', 'screening', 'alpha_int_H', 'apha_H_asym', 'alpha_H_oct_int', 'replace_LLm2_LL2_low_u',
+                               'itmax_asymmetric_calcs', 'alpha_rand_asymmetric_calcs', 'alpha_rho_asymmetric_calcs', 'alpha_reg_asym_calcs', 'uz_meV', 'uperp_meV']
+parameters_to_plot = {key: parameters_to_save[key] for key in parameters_in_plot_list}
+
+parameters_to_plot_text = []
+for key in sorted(list(parameters_to_plot.keys()), key=str.lower):
+    parameters_to_plot_text.append(str('%s: %s' % (key, parameters_to_plot[key])))
+
+parameters_to_folder_text = []
+for key in sorted(list(parameters_to_plot.keys()), key=str.lower):
+    parameters_to_folder_text.append(str('%s' % (parameters_to_plot[key])))
